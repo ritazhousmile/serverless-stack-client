b0e1b71d58a79cc53a9e046d3da7ba69b0b03707
ritazhousmile <ritazhousmile@gmail.com>
2019-12-04 23:19:38 -0500

    fix small bug on color picker

--- src/components/ChangeColor.js
+++ src/components/ChangeColor.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { CirclePicker } from 'react-color';
 import "./ChangeColor.css";
 
 export default function ChangeColor ({
   handler,
-  noteColor = noteColor,
+  noteColor = "#FFFFFF",
   ...props
 }){
   return (
