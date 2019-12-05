c31f90c644c0b190a1c6b5bf07f7ffe4540f9505
ritazhousmile <ritazhousmile@gmail.com>
2019-12-04 21:10:06 -0500

    initial styling

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 import { API } from "aws-amplify";
 import "./Home.css";
 
 export default function Home(props) {
   const [notes, setNotes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
 
   useEffect(() => {
   async function onLoad() {
@@ -56,9 +58,17 @@ export default function Home(props) {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
-      </div>
-    );
-  }
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
+    </div>
+  );
+}
 
   function renderNotes() {
     return (
